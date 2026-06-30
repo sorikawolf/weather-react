438c7202719be5a9e4f200029451a52816f07093
sorikawolf <jennsorikawolf@gmail.com>
2026-06-29 22:23:32 -0400

    edited wording in footer

--- src/Footer.js
+++ src/Footer.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 import "./Footer.css";
+
 export default function Footer() {
   return (
     <footer className="Footer">
@@ -8,7 +9,7 @@ export default function Footer() {
       <a href="https://github.com/sorikawolf" target="_blank" rel="noreferrer">
         Sorika Wolf
       </a>{" "}
-      and is hosted on{" "}
+      and is open-sourced on{" "}
       <a
         href="https://github.com/sorikawolf/weather-react"
         target="_blank"
